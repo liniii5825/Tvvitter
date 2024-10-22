57dcc4f64143116b4c9cedbd3495666611a79bfa
Lin_2z <liniii@foxmail.com>
2024-10-22 22:43:46 +0800

    set up Tvvitter project :)

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  connectMongoDB();
+});
